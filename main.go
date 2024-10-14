8c98a6a0042c4531a5ab35bcbe7980ff0ac64c31
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-10-14 07:51:06 +0530

    ✨ added direct object moment

--- main.go
+++ main.go
@@ -1,136 +1,187 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+    "fmt"
+    "math/rand"
+    "time"
 )
 
-var n = 50
-var mat = make([][]string, n)
-
+var n = 50 
 
 type Obj struct {
-	x   int
-	y   int
-	obj string
+    x     int
+    y     int
+    obj   string
+    prevX int
+    prevY int
 }
 
 func randRange(min, max int) int {
-	return rand.Intn(max-min) + min
+    return rand.Intn(max-min) + min
 }
 
-func printMat(m [][]string) {
-	for i := 0; i < len(m); i++ {
-		for j := 0; j < len(m[i]); j++ {
-			fmt.Print(m[i][j])
-		}
-		fmt.Println()
-	}
+
+func moveCursor(x, y int) {
+    fmt.Printf("\033[%d;%dH", x+1, y*2+1) 
 }
+
+
+func clearScreen() {
+    fmt.Print("\033[2J")
+}
+
+
+func hideCursor() {
+    fmt.Print("\033[?25l")
+}
+
+
+func showCursor() {
+    fmt.Print("\033[?25h")
+}
+
 
 func spawnObjects() []*Obj {
-	var objects []*Obj
-	for i := 0; i < 100; i++ {
-		x := randRange(1, n-2)
-		y := randRange(1, n-2)
-		mat[x][y] = "🪨"
-		objects = append(objects, &Obj{x, y, "🪨"})
-	}
-	for i := 0; i < 20; i++ {
-		x := randRange(1, n-2)
-		y := randRange(1, n-2)
-		mat[x][y] = "🔖"
-		objects = append(objects, &Obj{x, y, "🔖"})
-	}
-	for i := 0; i < 20; i++ {
-		x := randRange(1, n-2)
-		y := randRange(1, n-2)
-		mat[x][y] = "✂️"
-		objects = append(objects, &Obj{x, y, "✂️"})
-	}
-	return objects
-}
-func resolveCollision(a, b *Obj) string {
-	if a.obj == "🪨" && b.obj == "✂️" {
-		return "🪨"
-	} else if a.obj == "✂️" && b.obj == "🔖" {
-		return "✂️"
-	} else if a.obj == "🔖" && b.obj == "🪨" {
-		return "🔖"
-	} else if b.obj == "🪨" && a.obj == "✂️" {
-		return "🪨"
-	} else if b.obj == "✂️" && a.obj == "🔖" {
-		return "✂️"
-	} else if b.obj == "🔖" && a.obj == "🪨" {
-		return "🔖"
-	}
-	return a.obj
+    var objects []*Obj
+    for i := 0; i < 100; i++ {
+        x := randRange(1, n-2)
+        y := randRange(1, n-2)
+        objects = append(objects, &Obj{x, y, "🪨", x, y})
+        moveCursor(x, y)
+        fmt.Print("🪨")
+    }
+    for i := 0; i < 100; i++ {
+        x := randRange(1, n-2)
+        y := randRange(1, n-2)
+        objects = append(objects, &Obj{x, y, "🔖", x, y})
+        moveCursor(x, y)
+        fmt.Print("🔖")
+    }
+    for i := 0; i < 100; i++ {
+        x := randRange(1, n-2)
+        y := randRange(1, n-2)
+        objects = append(objects, &Obj{x, y, "✂️", x, y})
+        moveCursor(x, y)
+        fmt.Print("✂️")
+    }
+    return objects
 }
 
+
+func resolveCollision(a, b *Obj) string {
+    
+    if a.obj == "🪨" && b.obj == "✂️" {
+        return "🪨"
+    } else if a.obj == "✂️" && b.obj == "🔖" {
+        return "✂️"
+    } else if a.obj == "🔖" && b.obj == "🪨" {
+        return "🔖"
+    } else if b.obj == "🪨" && a.obj == "✂️" {
+        return "🪨"
+    } else if b.obj == "✂️" && a.obj == "🔖" {
+        return "✂️"
+    } else if b.obj == "🔖" && a.obj == "🪨" {
+        return "🔖"
+    }
+    return a.obj 
+}
+
+
 func moveObjects(objects []*Obj) {
-	for _, obj := range objects {
-		mat[obj.x][obj.y] = "  "
-		direction := rand.Intn(4)
-		switch direction {
-		case 0: 
-			if obj.x > 1 {
-				obj.x--
-			}
-		case 1:
-			if obj.x < n-2 {
-				obj.x++
-			}
-		case 2:
-			if obj.y > 1 {
-				obj.y--
-			}
-		case 3:
-			if obj.y < n-2 {
-				obj.y++
-			}
-		}
-		if mat[obj.x][obj.y] != "  " {
-			for _, other := range objects {
-				if other.x == obj.x && other.y == obj.y && other != obj {
-					// Resolve collision
-					winner := resolveCollision(obj, other)
-					obj.obj = winner
-					other.obj = winner
-				}
-			}
-		}
-		mat[obj.x][obj.y] = obj.obj
-	}
+    positions := make(map[[2]int]*Obj)
+    for _, obj := range objects {
+        
+        obj.prevX = obj.x
+        obj.prevY = obj.y
+
+        
+        direction := rand.Intn(4)
+        switch direction {
+        case 0: 
+            if obj.x > 1 {
+                obj.x--
+            }
+        case 1: 
+            if obj.x < n-2 {
+                obj.x++
+            }
+        case 2: 
+            if obj.y > 1 {
+                obj.y--
+            }
+        case 3: 
+            if obj.y < n-2 {
+                obj.y++
+            }
+        }
+
+        
+        posKey := [2]int{obj.x, obj.y}
+        if other, exists := positions[posKey]; exists && other != obj {
+            winner := resolveCollision(obj, other)
+            obj.obj = winner
+            other.obj = winner
+        }
+        positions[posKey] = obj
+    }
+
+    
+    for _, obj := range objects {
+        
+        moveCursor(obj.prevX, obj.prevY)
+        fmt.Print("  ")
+
+        
+        moveCursor(obj.x, obj.y)
+        fmt.Print(obj.obj)
+    }
+}
+
+
+func drawBorders() {
+    
+    for y := 0; y < n; y++ {
+        moveCursor(0, y)
+        if y == 0 {
+            fmt.Print("+")
+        } else if y == n-1 {
+            fmt.Print("+")
+        } else {
+            fmt.Print("--")
+        }
+    }
+    
+    for y := 0; y < n; y++ {
+        moveCursor(n-1, y)
+        if y == 0 {
+            fmt.Print("+")
+        } else if y == n-1 {
+            fmt.Print("+")
+        } else {
+            fmt.Print("--")
+        }
+    }
+    
+    for x := 1; x < n-1; x++ {
+        moveCursor(x, 0)
+        fmt.Print("|")
+        moveCursor(x, n-1)
+        fmt.Print("|")
+    }
 }
 
 func main() {
-	for i := range mat {
-		mat[i] = make([]string, n)
-	}
+    rand.Seed(time.Now().UnixNano())
+    clearScreen()
+    hideCursor()
+    defer showCursor()
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if i == 0 || i == n-1 { 
-				if j == 0 || j == n-1 {
-					mat[i][j] = "+"
-				} else {
-					mat[i][j] = "-"
-				}
-			} else if j == 0 || j == n-1 {
-				mat[i][j] = "|"
-			} else {
-				mat[i][j] = "  "
-			}
-		}
-	}
+    drawBorders()
 
-	objects := spawnObjects()
+    objects := spawnObjects()
 
-	for {
-		moveObjects(objects)
-		printMat(mat)
-		time.Sleep(500 * time.Millisecond)
-		fmt.Println("\n\n")
-	}
+    for {
+        moveObjects(objects)
+        time.Sleep(200 * time.Millisecond)
+    }
 }
